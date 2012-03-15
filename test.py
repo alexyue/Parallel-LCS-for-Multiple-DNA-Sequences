07ccb531babf2167d1fbadb9d8beb2d05b393216
Alex, Yue <Yue.Alex@gmail.com>
2012-03-14 21:26:25 -0500

    add new methods to calc lcs

--- test.py
+++ test.py
@@ -38,21 +38,36 @@ def reducefn(k, vs):
     return {"A": Ba}, {"C": Bc} , {"T": Bt}, {"G": Bg}
 
 def CalcXYZ(r, arr):
-    maxLength = max([len(arr[0]), len(arr[1]), len(arr[2])])
-    
-    #fill in missing lengths
-    for i in range(len(arr)):
-        l = len(arr[i])
-        if len(arr[i]) < maxLength:
-            s = maxLength - l
-            for j in range(s, maxLength):
-                arr[i].append(0)
+    X = 0
+    Y = 0
+    Z = 0
 
-    X = arr[0][r] + arr[1][r] + arr[2][r]
-    Y = math.fabs(arr[0][r] - arr[1][r]) + math.fabs(arr[1][r] - arr[2][r])  
+    l = len(r)
+    print r, arr
+    for i in range(l):
+        X += arr[i][r[i]]
+
+    for i in range(l - 1):
+        Y += math.fabs(arr[i][r[i]] - arr[i + 1][r[i]])
+
+   # X = arr[0][r] + arr[1][r] + arr[2][r]
+   # Y = math.fabs(arr[0][r] - arr[1][r]) + math.fabs(arr[1][r] - arr[2][r])  
     Z = X + Y
 
     return Z  
+
+
+
+def UnjagArray(arr):
+    maxLength = max([len(arr[0]), len(arr[1]), len(arr[2])])
+
+    #fill in missing lengths
+    for i in range(len(arr)):
+        l = len(arr[i])
+        if l < maxLength:
+            s = maxLength - l
+            for j in range(s):
+                arr[i].append(0)
 
 
 def GetIJK(r, arr):
@@ -61,6 +76,19 @@ def GetIJK(r, arr):
         newRow.append(arr[i][r])
 
     return newRow
+
+def GetNextIndexes(r, arr):
+    newIndexes = []
+    l = len(r) #num strings
+
+    for i in range(l):
+        for j in range(len(arr[i])):
+            if arr[i][j] > r[i]:
+                #newIndexes.append(arr[i][j])
+                newIndexes.append(j)
+                break
+
+    return newIndexes
 
 
 s = mincemeat.Server()
@@ -96,10 +124,17 @@ for k,v in results.iteritems():
     for s,t in v[3].iteritems():
         Bg.append(t)
 
-Za = CalcXYZ(0,Ba)
-Zc = CalcXYZ(0,Bc)
-Zt = CalcXYZ(0,Bt)
-Zg = CalcXYZ(0,Bg)
+UnjagArray(Ba)
+UnjagArray(Bc)
+UnjagArray(Bt)
+UnjagArray(Bg)
+
+initPos = [0,0,0]
+
+Za = CalcXYZ(initPos,Ba)
+Zc = CalcXYZ(initPos,Bc)
+Zt = CalcXYZ(initPos,Bt)
+Zg = CalcXYZ(initPos,Bg)
 
 newZ = {Za:"A", Zc:"C", Zt:"T", Zg:"G"}
 minZ = min(Za, Zc, Zt, Zg)
@@ -115,12 +150,35 @@ elif letter == "T":
 elif letter == "G":
     arr = Bg
 
-newRow = GetIJK(0, arr)
+print letter
 
-print newRow
-    
+newRow = GetIJK(0, arr) #fix this for more strings
+
+newPosa = GetNextIndexes(newRow, Ba)
+newPosc = GetNextIndexes(newRow, Bc)
+newPost = GetNextIndexes(newRow, Bt)
+newPosg = GetNextIndexes(newRow, Bg)
 
 
+Za = CalcXYZ(newPosa,Ba)
+Zc = CalcXYZ(newPosc,Bc)
+Zt = CalcXYZ(newPost,Bt)
+Zg = CalcXYZ(newPosg,Bg)
 
+newZ = {Za:"A", Zc:"C", Zt:"T", Zg:"G"}
+minZ = min(Za, Zc, Zt, Zg)
+
+letter = newZ[minZ]
+
+if letter == "A":
+    arr = Ba
+elif letter == "C":
+    arr = Bc
+elif letter == "T":
+    arr = Bt
+elif letter == "G":
+    arr = Bg
+
+print letter
 
 
